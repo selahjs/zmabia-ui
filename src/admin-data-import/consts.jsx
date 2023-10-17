6ec8ffedc06986383c519d7f7ffb75b7c00c99d2
mdulko <122530805+mdulko@users.noreply.github.com>
2023-10-17 10:09:05 +0200

    OLMIS-7673: input size, typo in info text (#28)
    * OLMIS-7673: Add import page
    
    * OLMIS-7673: changed info text, color of 'x' button, added navigation from breadcrumb
    
    * OLMIS-7673: input size, typo in info text
    
    * update

--- src/admin-data-import/consts.jsx
+++ src/admin-data-import/consts.jsx
@@ -17,6 +17,6 @@ export const TYPE_OF_IMPORTS = [
     {
         value: "Product",
         name: "Products",
-        info: "To import Products you need: orderable.csv, orderableIdentifier.csv, programOrderable.csv, tradeitem.csv"
+        info: "To import Products you need: orderable.csv, orderableIdentifier.csv, programOrderable.csv, tradeItem.csv"
     }
 ];
