1c475f3c5e37c03840bed019151f6e8845f5bc88
Hassan <hhassan.developer@gmail.com>
2024-08-19 11:11:00 +0300

    Added SVG UI deployment

--- src/react-hooks/useServerService.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useMemo } from 'react';
-import getService from '../react-components/utils/angular-utils';
-
-const useServerService = (serviceName) => {
-    return useMemo(() => getService(serviceName), []);
-}
-
-export default useServerService;
